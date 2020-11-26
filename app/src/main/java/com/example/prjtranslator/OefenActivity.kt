9018505a717c2ba203f098da3d7809d7ba63241c
TimBlommestijn <1043092@mborijnland.nl>
2020-11-26 16:52:42 +0100

    refactored de code voor beter overzicht

--- app/src/main/java/com/example/prjtranslator/OefenActivity.kt
+++ app/src/main/java/com/example/prjtranslator/OefenActivity.kt
@@ -2,21 +2,19 @@ package com.example.prjtranslator
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.annotation.GlideModule
-
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import org.json.JSONObject
 
 
 @GlideModule
 class OefenActivity : AppCompatActivity() {
+    private val db = Firebase.firestore
+    private val categoryRef = db.collection("catogories").document("dieren")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,26 +24,25 @@ class OefenActivity : AppCompatActivity() {
             val intent: Intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+        //call function to get data from firebase
         loadData();
     }
 
     fun loadData() {
-        // define db
-        val db = Firebase.firestore
-        val categoryref = db.collection("catogories").document("dieren")
-        var url = "";
-        var image: HashMap<String, *> = HashMap<String, String>()
-        categoryref.get().addOnSuccessListener { document ->
+        categoryRef.get().addOnSuccessListener { document ->
             if (document != null) {
-//                println("Success ${document.data}")
-                val result = document.data;
-                image = result?.get("images") as HashMap<String, *>
-                url = image.get("egel").toString()
-                val iv: ImageView = findViewById(R.id.imageQuestion)
-                Glide.with(this@OefenActivity).load(url).into(iv)
+                val image = document.data?.get("images") as HashMap<String, *>
+                // invoke function that updates the ui with the image hashmap
+                updateUi(image)
             }
         }.addOnFailureListener { exception ->
             println("Error ${exception}")
         }
+    }
+
+    fun updateUi(image: HashMap<String, *>){
+        var url = image.get("egel").toString()
+        val iv: ImageView = findViewById(R.id.imageQuestion)
+        Glide.with(this@OefenActivity).load(url).into(iv)
     }
 }

4b318ea3c7806c6802cc477ef689513dd17e5d8b
wulffZ <1042400@mborijnland.nl>
2020-12-07 12:18:24 +0100

    Begin gemaakt aan speel pagina. alles werkt behalve antwoorden geven

--- app/src/main/java/com/example/prjtranslator/SpeelActivity.kt
+++ app/src/main/java/com/example/prjtranslator/SpeelActivity.kt
@@ -1,20 +1,100 @@
 package com.example.prjtranslator
+
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.annotation.GlideModule
+import com.google.firebase.database.*
+import java.util.*
 
+
+@GlideModule
 class SpeelActivity : AppCompatActivity() {
+    private val database = FirebaseDatabase.getInstance();
+    private lateinit var myRef: DatabaseReference;
+    private lateinit var categoryname: String;
+    private val data = arrayListOf<SpeelActivity.Questions>();
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        categoryname =
+            intent.getStringExtra("com.example.prjtranslator.info").toString().capitalize();
+        myRef =
+            database.getReference("1eLp2DK8iDagiTPtyGeDDLv_Qk4V7K5bL4anCPoxQmYY/" + categoryname);
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.speel)
+
         val homeButton = findViewById<Button>(R.id.home)
         homeButton.setOnClickListener {
-            val intent : Intent = Intent(this, MainActivity::class.java)
+            val intent: Intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-        // intent from category choosing screen
-        var categoryname : String = intent.getStringExtra("com.example.prjtranslator.info").toString();
-        println("catname " + categoryname)
+
+        val reloadBtn = findViewById<Button>(R.id.reloadPageBtn);
+        reloadBtn.setOnClickListener {
+            addData();
+        }
+        getData();
     }
+
+    private fun getData() {
+        database.goOnline()
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val values = dataSnapshot.children
+                for (ds in values) {
+                    val categoryItems = ds.getValue(Questions::class.java);
+                    data.add(categoryItems!!);
+                }
+                addData();
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                println("Failed to read value " + error.toException())
+            }
+        })
+    }
+
+    private fun addData() {
+        val randomNumber = Random().nextInt(data.count()) // random item from db
+        val randomType = Random().nextInt(2) // text, or image
+
+        if (randomType == 0) {
+            val item: Button = findViewById(R.id.playBtn)
+            item.isEnabled = false
+            val iv: ImageView = findViewById(R.id.imageQuestion)
+            val textnaam = findViewById<TextView>(R.id.imgText);
+            textnaam.setText("Wat zie je?");
+            Glide.with(this@SpeelActivity).load(data.get(randomNumber).imgUrl.toString()).into(iv)
+        }
+        if (randomType == 1) {
+            val item: Button = findViewById(R.id.playBtn)
+            item.isEnabled = true
+            val iv: ImageView = findViewById(R.id.imageQuestion)
+            iv.setImageResource(0)
+            val textnaam = findViewById<TextView>(R.id.imgText);
+            textnaam.setText(data.get(randomNumber).naam);
+        }
+
+        val playBtn = findViewById<Button>(R.id.playBtn);
+        playBtn.setOnClickListener {
+            val mediaPlayer: MediaPlayer? = MediaPlayer().apply {
+                setDataSource(data.get(randomNumber).mp3Url)
+                prepare()
+                start()
+            }
+        }
+    }
+
+    data class Questions(
+        val naam: String = "",
+        val vertaling: String = "",
+        val imgUrl: String = "",
+        val mp3Url: String = ""
+    )
 }

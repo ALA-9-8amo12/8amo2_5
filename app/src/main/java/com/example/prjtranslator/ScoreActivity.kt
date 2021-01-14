fb0ec95e51ca466a60e598c31105087ee69b0548
TimBlommestijn <1043092@mborijnland.nl>
2021-01-14 20:03:04 +0100

    push data to new intent

--- app/src/main/java/com/example/prjtranslator/ScoreActivity.kt
+++ app/src/main/java/com/example/prjtranslator/ScoreActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.widget.Button
 
 class ScoreActivity : AppCompatActivity() {
+
+    private lateinit var cato:String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.score)
@@ -13,7 +16,7 @@ class ScoreActivity : AppCompatActivity() {
             val intent : Intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-        var categoryname : String = intent.getStringExtra("com.example.prjtranslator.info").toString();
-        println("catname " + categoryname)
+        cato = intent.getStringExtra("categoryName").toString()
+        println("oi");
     }
 }
--- app/src/main/java/com/example/prjtranslator/SpeelActivity.kt
+++ app/src/main/java/com/example/prjtranslator/SpeelActivity.kt
@@ -23,15 +23,21 @@ class SpeelActivity : AppCompatActivity() {
     private var questioncount: Int = 1;
     private var incorrectcount: Int = 0;
     private val data = arrayListOf<SpeelActivity.Questions>();
+    private lateinit var categoryName: String
+
+    private fun setCato(){
+        categoryName = intent.getStringExtra("com.example.prjtranslator.info").toString().capitalize();
+    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        categoryname =
-            intent.getStringExtra("com.example.prjtranslator.info").toString().capitalize();
-        myRef =
-            database.getReference("1eLp2DK8iDagiTPtyGeDDLv_Qk4V7K5bL4anCPoxQmYY/" + categoryname);
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.speel)
+
+        setCato()
+        myRef =
+            database.getReference("1eLp2DK8iDagiTPtyGeDDLv_Qk4V7K5bL4anCPoxQmYY/" + categoryName);
 
         val homeButton = findViewById<Button>(R.id.home)
         homeButton.setOnClickListener {
@@ -117,9 +123,15 @@ class SpeelActivity : AppCompatActivity() {
         val randomNumber = Random().nextInt(data.count()) // random item from db
         val randomType = Random().nextInt(2) // text, or image
         answer = data.get(randomNumber).vertaling;
-        if (questioncount == 10) {
+        if (questioncount == 9) {
             val intent: Intent = Intent(this, ScoreActivity::class.java)
-            startActivity(intent)
+            if(this::categoryName.isInitialized ) {
+                intent.putExtra("categoryName", categoryName)
+                intent.putExtra("wrongs", incorrectcount)
+            }else {
+                intent.putExtra("categoryName", "it failed")
+            }
+                startActivity(intent)
         }
         // all buttons
         val answer1: Button = findViewById(R.id.answer1)
@@ -142,9 +154,6 @@ class SpeelActivity : AppCompatActivity() {
         }
         val randomButtonNumb = Random().nextInt(6)
         AllAnswers[randomButtonNumb].setText(answer);
-
-
-
 
 
         if (randomType == 0) {
